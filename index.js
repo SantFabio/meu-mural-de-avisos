7d9eb3dc637a781523edd07f07fea3b7474885c0
Luiz Fábio Santana <f4biosantana@gmail.com>
2024-09-10 15:17:21 -0300

    Primeira versão do projeto

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const path = require("path");
+const apiRoute = require('./routes/api')
+
+app.use("/api", apiRoute); //Essa linha deve ser chamada primeiro
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+    console.log("Server running on port ", PORT);
+});
+
---
+++ public/scripts/script.js
@@ -0,0 +1,48 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    updatePosts();
+})
+function updatePosts() {
+    fetch("http://192.168.3.50:3000/api/all")
+        .then(res => {
+            return res.json();
+        }).then(json => {
+            console.log(json);
+            let postElements = "";
+            let posts = json;
+            posts.forEach(post => {
+                let postElement =
+                    `<div id=${post.id} class="card mb-4">
+                        <div class="card-header">
+                            <h5 class="card-title">${post.title}</h5>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-text">${post.description}</div>
+                        </div>
+                </div>`;
+                postElements += postElement
+            });
+            document.getElementById("post").innerHTML = postElements;
+        })
+}
+function newPost() {
+    let title = document.getElementById("title").value;
+    let description = document.getElementById("desc").value;
+    // console.log(title, description);
+    let post = { title, description }
+    const options = {
+        method: "POST",
+        headers: new Headers({
+            "Content-Type": "application/json"
+        }),
+        body: JSON.stringify(post)
+    }
+    fetch("http://192.168.3.50:3000/api/new", options)
+        .then(res => {
+            console.log(res);
+            updatePosts();
+            document.getElementById("title").value = "";
+            document.getElementById("desc").value = "";
+        })
+
+}
